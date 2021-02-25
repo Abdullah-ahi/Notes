0f58ee8ae69c476be65662943f01401ceab7ed1d
abby <www.sar.ras@gmail.com>
2021-02-25 21:37:12 +0300

    updated server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-app.use('/', express.static('./src'));
 
 app.get('/notes', (req, res) => {
   fs.readFile('./server/db/notes.json', 'utf-8', (err, data) => {
